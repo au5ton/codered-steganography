2b8065a951a9d1c5d1d079aad878f714b87c5996
Austin Jackson <au5ton@icloud.com>
2015-11-21 18:12:21 -0600

    Added some planned methods

--- stego.js
+++ stego.js
@@ -1,8 +1,8 @@
 var stego = {};
 
 
-
-stego.parseImageBuffer = function(buffer) {
+//Takes a png.js image buffer and turns it into a pixel array
+stego.parseImageBufferToPixelArray = function(buffer) {
 
     var image = [];
 
@@ -14,6 +14,54 @@ stego.parseImageBuffer = function(buffer) {
     }
 
     return image;
+};
+
+
+stego.encodeDataFromBuffer = function(buffer, data) {
+
+    for(var i = 0; i < buffer.data.length; i++) {
+
+
+
+
+    }
+
+
+};
+
+stego.decodeDataFromBuffer = function(buffer, data) {
+
+};
+
+
+
+
+
+
+//Takes a pixel array and encodes binary data into the pixel array
+stego.encodeDataFromPixelArray = function(pixelArray, data) {
+
+    var binaryArrayOfData = [];
+    for(var i = 0; i < data.length; i++) {
+        data.charCodeAt(i).toString(2)
+    }
+
+    for(var i = 0; i < pixelArray.length; i++) {
+        pixelArray[i].r
+    }
+
+
+};
+
+//Takes a pixel array and encodes binary data into the pixel array
+stego.decodeDataFromPixelArray = function(pixelArray) {
+
+
+    for(var i = 0; i < pixelArray.length; i++) {
+        //
+    }
+
+
 };
 
 
