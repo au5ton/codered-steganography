803f5c1b1adfd77ae48e6e6c9676ba7c035726ff
Matt Nyman <matthew.nyman@yahoo.com>
2015-11-22 06:02:01 -0600

    added aes-256-cbc encryption

--- webserver.js
+++ webserver.js
@@ -3,6 +3,7 @@ var PNG = require('pngjs').PNG;
 var express = require('express');
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
+var crypto = require('crypto');
 var app = express();
 
 var stego = require('./stego');
@@ -19,7 +20,7 @@ app.get('/encode', function (req, res) {
     res.sendFile('encode.html', {root: './static'});
 });
 
-app.post('/encode', upload.array('photos', 2), function (req, res, next) {
+app.post('/encode', upload.array('files', 2), function (req, res, next) {
 
     console.log(req.files);
     console.log(req.body);
@@ -36,20 +37,27 @@ app.post('/encode', upload.array('photos', 2), function (req, res, next) {
         })).on('parsed', function() {
 
             //stego.reformatPixelArrayToBufferData();
-
+            var pw = req.body.pw || null;
             var processed;
             if(req.body.encode_type === 'text') {
+                var text = req.body.text_data;
                 console.log('Processing text.');
-                processed = stego.encodeDataFromPixelArray(stego.parseImageBufferToPixelArray(this), req.body.text_data, 'text');
+                if (pw){
+                    var encrypt = crypto.createCipher('aes-256-cbc', pw);
+                    text = encrypt.update(text, 'utf8', 'binary') + encrypt.final('binary');
+                }
+                //console.log(text);
+                processed = stego.encodeDataFromPixelArray(stego.parseImageBufferToPixelArray(this), text, 'text');
             }
             else if(req.body.encode_type === 'binary') {
                 console.log('Processing a binary file.');
                 var buffer = fs.readFileSync(req.files[1].path);
-                var stringified = JSON.stringify([req.files[1].originalname, buffer.toString('base64')]);
+                var stringified = JSON.stringify([req.files[1].originalname, buffer.toString('binary')]);
+                if (pw){
+                    var encrypt = crypto.createCipher('aes-256-cbc', pw);
+                    stringified = encrypt.update(stringified, 'binary', 'binary') + encrypt.final('binary');
+                }
                 processed = stego.encodeDataFromPixelArray(stego.parseImageBufferToPixelArray(this), stringified, 'text');
-            }
-            else {
-                console.log('something else??')
             }
 
             var n = 0;
@@ -95,23 +103,49 @@ app.post('/decode', upload.single('original_image'), function (req, res, next) {
     })).on('parsed', function() {
 
         var decoded = stego.decodeDataFromPixelArray(stego.parseImageBufferToPixelArray(this), req.body.expected_type);
+        var pw = req.body.pw || null;
 
         if(decoded.dataType === 'text') {
-            res.send(decoded.data);
+            var text = decoded.data;
+            try {
+                if (pw) {
+                    var decrypt = crypto.createDecipher('aes-256-cbc', pw);
+                    text = decrypt.update(text, 'binary', 'utf8') + decrypt.final('utf8');
+                }
+                //console.log(text)
+                res.send(text);
+            }
+            catch (err) {
+                res.send('Bad text password');
+            }
         }
         else {
-            console.log(decoded.data);
-            var parsed = JSON.parse(decoded.data);
-            console.log(parsed);
-            //parsed[0] file name
-            //parsed[1] the buffer as a base64 string
-
-            fs.writeFile(parsed[0]+'.bin', new Buffer(parsed[1], 'base64'), 'binary', function(err){
-                res.setHeader('Content-disposition', 'attachment; filename='+parsed[0]);
-                res.sendFile(parsed[0]+'.bin', {root: '.'});
-                //TODO: delete the file
-            });
-
+            
+            try {
+                var data = decoded.data;
+                if (pw) {
+                    //console.log('entering try block, pw : ' + pw);
+                    var decrypt = crypto.createDecipher('aes-256-cbc', pw);
+                    //console.log('decrypt made');
+                    data = decrypt.update(decoded.data, 'binary', 'binary') + decrypt.final('binary');
+                }
+                //console.log('data decoded');
+                //console.log(data);
+                var parsed = JSON.parse(data);
+                //console.log('data parsed');
+                //parsed[0] file name
+                //parsed[1] the buffer as a base64 string
+                var filename = parsed[0];
+                data = new Buffer(parsed[1], 'binary');
+                fs.writeFile(filename+'.bin', data, 'binary', function(err){
+                    res.setHeader('Content-disposition', 'attachment; filename='+filename);
+                    res.sendFile(filename, {root: '.'});
+                    //TODO: delete the file
+                });
+            }
+            catch (err) {
+                res.send('Bad bin password');
+            }
         }
     });
 });
