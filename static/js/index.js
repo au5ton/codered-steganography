aa5df62a6a297dcf4564bcf10b2b3f42e8ff3242
Austin Jackson <au5ton@icloud.com>
2015-11-22 06:41:03 -0600

    super complex form that doesn't work

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,69 @@
 //Index.js
 
+var $mode_select = $('#mode_select');
 var $encode_form = $('#encode_form');
 var $decode_form = $('#decode_form');
+
+$encode_form.hide();
+$decode_form.hide();
+disableEncodeForm();
+disableDecodeForm();
+
+var n = 0;
+
+$mode_select.on('change', function(event){
+    if(n === 0) {
+        n++;
+        $encode_form.show();
+        $decode_form.show();
+    }
+    if(event.target.selectedIndex === 1) {
+        enableEncodeForm();
+        disableDecodeForm();
+    }
+    if(event.target.selectedIndex === 2) {
+        disableEncodeForm();
+        enableDecodeForm();
+    }
+});
+
+$('#encode_password_protection').on('change', function(event) {
+    if($(event.target).is(':checked') === true) {
+        enableEncodePasswordField();
+    }
+    else {
+        disableEncodePasswordField();
+    }
+});
+$('#decode_password_protection').on('change', function(event) {
+    if($(event.target).is(':checked') === true) {
+        enableDecodePasswordField();
+    }
+    else {
+        disableDecodePasswordField();
+    }
+});
+
+function disableEncodeForm() {
+    $encode_form.css('opacity',0.25);
+    $('[name=\'encode_type\']').attr('disabled','');
+    $('#encode_form .btn').addClass('disabled');
+}
+
+function enableEncodeForm() {
+    $encode_form.css('opacity',1);
+    $('[name=\'encode_type\']')[0].removeAttribute('disabled');
+    $('#encode_form .btn').removeClass('disabled');
+}
+
+function disableDecodeForm() {
+    $decode_form.css('opacity',0.25);
+    $('[name=\'expected_type\']').attr('disabled','');
+    $('#decode_form .btn').addClass('disabled');
+}
+
+function enableDecodeForm() {
+    $decode_form.css('opacity',1);
+    $('[name=\'expected_type\']')[0].removeAttribute('disabled');
+    $('#decode_form .btn').removeClass('disabled');
+}
